48dad84d1a61a45e129ceb2e9ea05496e17dd1cd
Alumno <alumno@cepibase.int>
2023-10-24 13:17:43 +0200

    agrega una nueva funcion

--- principal.py
+++ principal.py
@@ -7,6 +7,11 @@ from pymongo import MongoClient
 from bson import ObjectId
 from flask_mail import Mail, Message
 
+def comprueba_fondo():
+    pass
+
+DOCUMENTOS = ["doc", "docs"]
+
 EXTENSIONES = ["png", "jpg", "jpeg"]
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "./static/fondos"
